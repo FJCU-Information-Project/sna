4dd576600ac6dd1f7dec0a3c24bba5153ea6a383
smallella <ella5898g5@gmail.com>
2021-11-13 12:08:23 +0800

    Update gov_traffic.R

--- gov_traffic.R
+++ gov_traffic.R
@@ -7,15 +7,28 @@ edges <- jsonlite::fromJSON("https://raw.githubusercontent.com/datastorm-open/da
 #traffic project node and edge link
 nodes <- jsonlite::fromJSON("https://raw.githubusercontent.com/FJCU-Information-Project/sna/main/gov_nodes.json")
 edges <- jsonlite::fromJSON("https://raw.githubusercontent.com/FJCU-Information-Project/sna/main/gov_edges.json")
-id->c(0:19)
+id->c(0:19) #20列id
 ID->data.frame(id)
 nodes<-nodes[0:20,c("區","肇事因素個別")]
-traffic_nodes<-cbind(ID,nodes)
-colnames(traffic_nodes)<-c("id","label","group")
+traffic_nodes<-cbind(ID,nodes) #將id欄和node欄合併
+# setnames(資料,舊變數名稱,新變數名稱)
+#setnames(mydata, c("dest","origin"), c("Destination", "origin.of.flight"))
+colnames(traffic_nodes)<-c("id","label","group") #欄位重新命名
 traffic_edges<-edges[0:20,]
-visNetwork(traffic_nodes, traffic_edges, height = "700px", width = "100%") %>%
+#每個區域代表不同的case 連結在一起的node代表是相同的肇事因素
+visNetwork(traffic_nodes, traffic_edges,  
+           main = "台中交通事故sna", 
+           submain = list(text = "個別肇事因素與區域關聯圖",
+                          style = "font-family:Comic Sans MS;color:#ff0000;font-size:15px;text-align:center;"), 
+           footer = "Fig.1 依區域分為不同的case",
+           height = "700px", width = "100%") %>%
   visOptions(selectedBy = "group", 
-             manipulation = TRUE,
+             manipulation = TRUE, #edit network
              highlightNearest = TRUE, 
              nodesIdSelection = TRUE) %>%
-  visPhysics(stabilization = FALSE)
+  visGroups(groupname = "A", color = "red") %>%
+  visGroups(groupname = "B", color = "lightblue") %>%
+  visLegend()%>%
+  visPhysics(stabilization = FALSE,#動態效果
+             solver = "repulsion", 
+             repulsion = list(gravitationalConstant = -500))
