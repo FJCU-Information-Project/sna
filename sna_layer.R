2b6c0c20111995f3d3ec9171c88ccf657f29d9e7
im39user <im39user@outlook.com>
2022-02-06 15:07:30 +0800

    update

--- sna_layer.R
+++ sna_layer.R
@@ -1,56 +1,51 @@
 #ctrl shift + c 多行註解
 
-print("Hello Before library")
+
 
 library(visNetwork)
 library(RMySQL)
 library(igraph)
 library(utf8)
 layer_csv<- read.csv(file = 'E:/GitHub/sna/layer.csv', encoding = "UTF-8")
-names(layer_csv)[1] <- "factor_id"
+names(layer_csv)[1] <- "factor_id"#將第一個欄名變更
 
 connect = dbConnect(MySQL(), dbname = "trans",username = "root",
                     password = "IM39project",host = "140.136.155.121",port=50306,DBMSencoding="UTF8")
 dbListTables(connect)
 dbSendQuery(connect,"SET NAMES BIG5") # 設定資料庫連線編碼
 Sys.getlocale(category = "LC_ALL") # 查詢系統編碼環境
-#layer_from_id<-data.frame(id = (layer_csv$factor_id))
-#duplicated_from_id<-data.frame(id=(layer_from_id[!duplicated(layer_from_id),]),color=c(layer_from_id$color))
-layer_to_id<-data.frame(id = c(layer_csv$near_id),group=c(layer_csv$level),color=c(layer_csv$color),total=c(layer_csv$weight))
-#bind_from_to_id<-rbind(duplicated_from_id,layer_to_id)
-#duplicated_bind_from_to_id<-data.frame(id=(bind_from_to_id[!duplicated(bind_from_to_id),]))
+layer_to_id<-data.frame(from_id = c(layer_csv$factor_id),id = c(layer_csv$near_id),group=c(layer_csv$level),color=c(layer_csv$color),total=c(layer_csv$weight))
 
+node_layer<- dbGetQuery(connect ,"select * from `node`")
 attr_layer<- dbGetQuery(connect ,"select * from `attribute`")
 names(attr_layer)[1] <- "attribute"
 names(attr_layer)[2] <- "attr_name"
-get_layer_attr<- merge(x = node_layer, y = attr_layer, by = "attribute", all.x = TRUE)#left join
+get_layer_attr<- merge(x = node_layer, y = attr_layer, by = "attribute", all.x = TRUE)#將屬性和節點表格合併
+all_layer_node<- merge(x = layer_to_id, y = get_layer_attr, by = "id", all.x = TRUE)#用id合併得到節點的屬性資訊
 
-all_layer_node<- merge(x = layer_to_id, y = get_layer_attr, by = "id", all.x = TRUE)#left join
-#duplicated(all_layer_node$(id,group))
-all_layer_node<-unique(all_layer_node)
-all_layer_node<-all_layer_node[order(all_layer_node$id,all_layer_node$group),]
+all_layer_node<-unique(all_layer_node)#刪除重複的第二層節點
+all_layer_node<-all_layer_node[order(all_layer_node$id,all_layer_node$group),]#將層級做降冪排列
+all_layer_node <-all_layer_node[(!duplicated(all_layer_node$id)),]#刪除和第一層重複的第二層節點
 
-all_layer_node <-all_layer_node[(!duplicated(all_layer_node$id)),]
-
-
+#用於畫sna圖的節點
 layer_nodes<- data.frame(id = c(all_layer_node$id), group = c(all_layer_node$group), 
-                         label = paste(all_layer_node$group), 
-                         title = paste("<p>", all_layer_node$attr_name,"<br>",all_layer_node$enname,"</p>"),
+                         label = paste(all_layer_node$name), 
+                         title = paste("<p>", all_layer_node$name,"<br>", all_layer_node$attr_name,"<br>",all_layer_node$enname,"</p>"),
                          font.size = 20,
                          color=c(all_layer_node$color)
                          )
-
+#用於畫sna圖的關聯
 layer_relationship<- data.frame(from = c(layer_csv$factor_id)
                                 ,to = c(layer_csv$near_id)
                                 ,value = c(layer_csv$weight)
                                 ,font.size = 10
                                 ,label = paste("權重", layer_csv$weight)
                                 ,font.color ="brown")
-#print(total_nodes)
+print(total_nodes)
 draw_sna_layer<-visNetwork(layer_nodes,layer_relationship, width = "100%", height = "500px")%>%
   visNodes(size = 30)%>%
   visOptions(highlightNearest = TRUE
-             , selectedBy= "label"
+             , selectedBy= "group"
              ,nodesIdSelection = list(enabled = TRUE
                                       ,style = 'width: 200px; height: 26px;
                                   background: #f8f8f8;
@@ -61,15 +56,25 @@ draw_sna_layer<-visNetwork(layer_nodes,layer_relationship, width = "100%", heigh
              solver = "repulsion",
              repulsion = list(gravitationalConstant = 1500))
 
-visSave(draw_sna_layer, file = "../flask/templates/layer.html")
+visSave(draw_sna_layer, file = "/layer.html")
 
+from_layer_id<-all_layer_node
+names(from_layer_id)[2] <- "from_id"
+get_layer_from_attr<-get_layer_attr
+names(get_layer_from_attr)[2] <- "from_id"
+all_from_layer_node<- merge(x = from_layer_id, y = get_layer_from_attr, by = "from_id", all.x = TRUE)#用id合併得到節點的屬性資訊
 
-layerTable<- data.frame(node_id = c(all_layer_node $id)
-                       ,node_name = c(all_layer_node $name)
-                       ,node_attr_name = c(all_layer_node $attr_name)
-                       ,node_eng_name = c(all_layer_node $enname)
-                       ,node_weight=c(all_layer_node $total)
-                       ,node_layer=c(all_layer_node $group)
+#層級分析csv表(欄位:節點from_id,from節點名稱,from節點屬性,from節點英文名,節點to_id,to節點名稱,to節點屬性,to節點英文名,權重,第幾層)
+layerTable<- data.frame(node_from_id = c(all_from_layer_node $from_id)
+                        ,node_from_name = c(all_from_layer_node $name.x)
+                        ,node_from_attr_name = c(all_from_layer_node $attr_name.x)
+                        ,node_from_eng_name = c(all_from_layer_node $enname.x)
+                        ,node_to_id = c(all_layer_node $id)
+                        ,node_to_name = c(all_layer_node $name)
+                        ,node_to_attr_name = c(all_layer_node $attr_name)
+                        ,node_to_eng_name = c(all_layer_node $enname)
+                        ,node_weight=c(all_layer_node $total)
+                        ,node_layer=c(all_layer_node $group)
                        )
 
-write.csv(rankTable,"../flask/layerTable.csv", row.names = FALSE, fileEncoding = "UTF-8")
+write.csv(layerTable,"../flask/layerTable.csv", row.names = FALSE, fileEncoding = "UTF-8")
