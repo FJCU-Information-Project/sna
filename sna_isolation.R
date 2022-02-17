9187e95dd0eda0f39396025908fc8d96d652ef32
im39user <im39user@outlook.com>
2022-02-17 16:20:18 +0800

    isolation_021701

--- sna_isolation.R
+++ sna_isolation.R
@@ -19,9 +19,12 @@ dbListTables(connect)
 dbSendQuery(connect,"SET NAMES BIG5") # 設定資料庫連線編碼
 Sys.getlocale("LC_ALL") #解決中文編碼問題
 print("21")
-select_isolation <- data.frame(c(dbGetQuery(connect ,("SELECT * FROM trans.default_relationship where from_id = ",inId," and total = 0"))))
-from_id_name <- data.frame(c(dbGetQuery(connect ,("select from_id,`name` from trans.node n,trans.default_relationship d where from_id = ",inId," and total = 0 and n.id = d.from_id"))))
-to_id_name <- data.frame(c(dbGetQuery(connect ,("select to_id,`name` from trans.node n,trans.default_relationship d where from_id = ",inId," and total = 0 and n.id = d.to_id"))))
+print(inId)
+select_isolation <- data.frame(c(dbGetQuery(connect ,paste("SELECT * FROM trans.default_relationship where from_id = ",inId," and total = 0"))))
+
+print(select_isolation)
+from_id_name <- data.frame(c(dbGetQuery(connect ,paste("select from_id,`name` from trans.node n,trans.default_relationship d where from_id = ",inId," and total = 0 and n.id = d.from_id"))))
+to_id_name <- data.frame(c(dbGetQuery(connect ,paste("select to_id,`name` from trans.node n,trans.default_relationship d where from_id = ",inId," and total = 0 and n.id = d.to_id"))))
 print("25")
 node <- data.frame(id=c(select_isolation[1,1],select_isolation[,2]),label = c(from_id_name[1,2],to_id_name[,2]),title = c(from_id_name[1,2],to_id_name[,2]),font.size = 20)
 #edge <- data.frame(from=c(select_isolation[,1]), to=c(select_isolation[,2]), value=c(select_isolation[,3]))
@@ -39,8 +42,8 @@ isolation_pic <- visNetwork(node, width = "100%", height = "500px")%>%
              solver = "repulsion",
              repulsion = list(gravitationalConstant = 1500))
 
-visSave(isolation_pic, file = "../flask/templates/result.html",selfcontained = FALSE, background = "white")
+visSave(isolation_pic, file = "../flask/templates/isolation.html",selfcontained = FALSE, background = "white")
 print("43")
 isolation_table <- data.frame(from_id=c(select_isolation$from_id),from_id_name=c(from_id_name$name),to_id=c(select_isolation$to_id),to_id_name=c(to_id_name$name),total=c(select_isolation$total))
-write.csv(isolation_table,"../flask/result_table.csv", row.names = FALSE, fileEncoding = "UTF-8")
+write.csv(isolation_table,"../flask/isolation_table.csv", row.names = FALSE, fileEncoding = "UTF-8")
 print("success")
