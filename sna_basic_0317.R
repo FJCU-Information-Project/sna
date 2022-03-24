d85ab3d02f395e639e9a38c6710af2b389da945a
ShihMin-Chien <jason8992825@gmail.com>
2022-03-24 17:06:05 +0800

    0324

--- sna_basic_0317.R
+++ sna_basic_0317.R
@@ -12,7 +12,7 @@ library(sna)
 
 args <- commandArgs(trailingOnly = TRUE)
 #inId <- args[1] # CLI input parameter
-inId <- 17
+#inId <- 17
 
 connect = dbConnect(MySQL(), dbname = "trans",username = "root",
                     password = "IM39project",host = "140.136.155.121",port=50306,DBMSencoding="UTF8")
@@ -23,6 +23,7 @@ Sys.getlocale("LC_ALL") #解決中文編碼問題
 #建立權重表矩陣
 the_max_node <- as.numeric(dbGetQuery(connect,"select max(`id`) from trans.node"))#取出節點最大值
 data <- dbGetQuery(connect,"select * from trans.default_relationship")
+#node_name <- dbGetQuery(connect,"select * from trans.node")
 data_count <- dbGetQuery(connect,"select count(total) from trans.default_relationship")#取出有可能的關聯組數
 data_matrix <- matrix(c(0),nrow = the_max_node,ncol = the_max_node)#建立一張全部為0的矩陣表
 i <- 1
@@ -35,8 +36,15 @@ while (i<=data_count) {
   i <- i + 1
 }
 
-basic_table <- data.frame(node_id=c(1:the_max_node),degree_centrality=c(degree(data_matrix,gmode = "graph",rescale = "TRUE")))
-basic_table <- subset(basic_table,degree_centrality != "NA" & degree_centrality != 0 ) #剔除NA和0的值
+basic_table <- data.frame(node_id=c(1:the_max_node),node_name=c(1:the_max_node),degree_centrality=c(degree(data_matrix,gmode = "graph",rescale = "TRUE")),closeness_centrality=c(closeness(data_matrix,gmode = "TRUE",cmode="suminvundir")))
+j <- 1
+while(j<=the_max_node){
+  node_name <- data.frame(c(dbGetQuery(connect,paste("select `name` from trans.node where id = ",j))))
+  basic_table[j,2] <- node_name[1,1]
+  j <- j + 1
+}
 
-write.csv(basic_table,"../flask/basic_table.csv", row.names = FALSE, fileEncoding = "UTF-8")
-write.csv(data_matrix,"../flask/data.csv", row.names = FALSE, fileEncoding = "UTF-8")
+basic_table <- subset(basic_table,degree_centrality != "NA" & degree_centrality != 0 & closeness_centrality != "NA" & closeness_centrality != 0) #剔除NA和0的值
+
+write.csv(basic_table,"C:/Users/User/Desktop/basic_table.csv", row.names = FALSE, fileEncoding = "UTF-8")
+write.csv(data_matrix,"C:/Users/User/Desktop/data_table.csv", row.names = FALSE, fileEncoding = "UTF-8")
