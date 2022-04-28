b8b34d12d6565ba7a192b33e65571469103594ba
im39user <im39user@outlook.com>
2022-04-29 01:38:21 +0800

    shihmin_0428

--- sna_all.R
+++ sna_all.R
@@ -12,8 +12,8 @@ args <- commandArgs(trailingOnly = TRUE)
 
 inId1 <- args[1] #(資料庫名稱，使用者代號user_id)
 inId2 <- args[2] #(資料表名稱，dataset_id)
-inId3 <- args[3] #選取想看的權重程度(前10%、前20%、前30%.....全部(100%))，以小數位回傳(0.1、0.2、0.3、....、1)
-inId3 <- as.numeric(inId3)
+#inId3 <- args[3] #選取想看的權重程度(前10%、前20%、前30%.....全部(100%))，以小數位回傳(0.1、0.2、0.3、....、1)
+#inId3 <- as.numeric(inId3)
 connect = dbConnect(MySQL(), dbname = inId1,username = "root",
                     password = "IM39project",host = "140.136.155.121",port=50306,DBMSencoding="UTF8")
 dbListTables(connect)
@@ -22,7 +22,8 @@ Sys.getlocale("LC_ALL") #解決中文編碼問題
 print("21")
 
 max_weight <- as.numeric(dbGetQuery(connect, paste0("select max(`total`) from `",inId1,"`.weight where dataset = ",inId2)))
-max_weight <- max_weight*(1-inId3)
+#max_weight <- max_weight*(1-inId3)
+max_weight <- max_weight*(1-0.2) #目前直接設定顯示關聯程度前20%
 node_all <- dbGetQuery(connect, paste0("select * from `",inId1,"`.node where dataset = ",inId2))
 node_id <- dbGetQuery(connect, paste0("select * from `",inId1,"`.weight where (total > ",max_weight,") and dataset = ",inId2))
 node_id <- data.frame(from_id=c(node_id[,2]), to_id=c(node_id[,3]), weight=c(node_id[,4]))
